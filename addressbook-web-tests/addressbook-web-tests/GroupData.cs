25ef7da2b5ab8bd7622699e23ca2c81a7d3c8f99
toliar <Toliar@mail.ru>
2017-04-10 15:34:32 +0300

    Task3

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class GroupData
+    {
+      //  private string name;
+      //  private string header=""; поле
+      // private string footer = "";
+        public GroupData(string name)
+        //свойство Name
+        {
+            this.Name = name;
+        }
+    //    public GroupData(string name,string header, string footer)
+    //    {
+   //         this.Name = name;
+    //        this.Header = header;
+   //         this.Footer = footer;
+   //     }
+        public string Name { get; set; }
+        public string Header { get; set; } = "";
+        public string Footer { get; set; } = "";
+    }
+}
