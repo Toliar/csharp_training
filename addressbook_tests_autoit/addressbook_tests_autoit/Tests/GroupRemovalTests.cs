0cabbc7b92a9050f869df1fe3ad24cb4e366f6ae
toliar <Toliar@mail.ru>
2017-05-31 14:54:02 +0300

    Task 18

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Tests/GroupRemovalTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        private int index=0;
+        [Test]
+        public void TestGroupRemoval()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            
+
+            app.Groups.Remove(index);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(index);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            OpenGroupsDialogue();
+
+            string count = aux.ControlTreeView(
+                GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.a6ad9a1", "GetItemCount", "#0","");
+            for (int i=0;i < int.Parse(count); i++)
+            {
+               string item = aux.ControlTreeView(
+                GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.a6ad9a1", "GetText", "#0|#"+i, "");
+                list.Add(new GroupData() {
+                Name= item});
+            }
+            CloseGroupsDialogue();
+            return list;
+        }
+
+        internal void Remove(int index)
+        {
+            OpenGroupsDialogue();
+            SelectGroupToDelete(index);
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.a6ad9a1"); // click delete button
+            aux.ControlClick("Delete group", "", "WindowsForms10.BUTTON.app.0.a6ad9a3");
+            CloseGroupsDialogue();
+
+        }
+
+        private void SelectGroupToDelete(int index)
+        {
+            aux.ControlTreeView(
+                GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.a6ad9a1", "Select", "#0|#" + index, "");
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            OpenGroupsDialogue();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.a6ad9a3");
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            CloseGroupsDialogue();
+        }
+
+        private void CloseGroupsDialogue()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.a6ad9a4");
+        }
+
+        private void OpenGroupsDialogue()
+        {
+            aux.ControlClick(WinTitle, "", "WindowsForms10.BUTTON.app.0.a6ad9a12");
+            aux.WinWait(GROUPWINTITLE);
+        }
+    }
+}
